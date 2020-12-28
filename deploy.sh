86cbc4a0320dd4550850ce747348b63ba92eec80
jfmaestr <jacobmaestri@gmail.com>
2020-12-27 22:51:13 -0600

    take 2 failed deploy

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 hugo -t book # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
-cd public
+cd public2
 
 # Add changes to git.
 git add .
